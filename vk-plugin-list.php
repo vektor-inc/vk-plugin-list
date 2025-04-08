380d2f64206fd381d679af63446f19ee0de73a0c
kurudrive <kurudrive@gmail.com>
2025-04-08 18:02:19 +0900

    フィルターフック追加

--- vk-plugin-list.php
+++ vk-plugin-list.php
@@ -14,24 +14,29 @@ if ( ! class_exists( 'VK_Plugin_List' ) ) {
     class VK_Plugin_List {
 
         // 除外するプラグイン
-        private $excluded_plugins = array(
-            'VK Plugin List',
-            'VK FullSite Exporter',
-        );
+        private $excluded_plugins;
 
         public function __construct() {
+            // 除外プラグインの初期値を設定
+            $this->excluded_plugins = array(
+                'VK Plugin List',
+                'VK FullSite Exporter',
+            );
+            // フィルターフックで除外プラグインを改変可能
+            $this->excluded_plugins = apply_filters( 'vk_plugin_list_excluded', $this->excluded_plugins );
+
             add_shortcode( 'vk-plugin-list', array( $this, 'render_plugin_list' ) );
         }
 
         /**
-         * プラグインリストを取得して出力する
+         * プラグインリストを取得してフィルタリングする
+         *
+         * @return array フィルタリングされたプラグインリスト
          */
-        public function render_plugin_list() {
+        private function get_filtered_plugins() {
             // すべてのプラグインを取得
             $all_plugins = get_plugins();
-            $output = '<table class="vkpl_table vk-table--mobile-block">';
-            $output .= '<thead><tr><th>Plugin Name</th><th>Description</th></tr></thead>';
-            $output .= '<tbody>';
+            $filtered_plugins = array();
 
             foreach ( $all_plugins as $plugin_file => $plugin_data ) {
                 // 除外プラグインをスキップ
@@ -39,6 +44,24 @@ if ( ! class_exists( 'VK_Plugin_List' ) ) {
                     continue;
                 }
 
+                $filtered_plugins[$plugin_file] = $plugin_data;
+            }
+
+            // フィルターフックでプラグインリストを改変可能
+            return apply_filters( 'vk_plugin_list_array', $filtered_plugins );
+        }
+
+        /**
+         * プラグインリストをHTMLとして出力する
+         *
+         * @return string HTML出力
+         */
+        private function render_plugin_list_html( $plugins ) {
+            $output = '<table class="vkpl_table vk-table--mobile-block">';
+            $output .= '<thead><tr><th>Plugin Name</th><th>Description</th></tr></thead>';
+            $output .= '<tbody>';
+
+            foreach ( $plugins as $plugin_file => $plugin_data ) {
                 $plugin_name = esc_html( $plugin_data['Name'] );
                 $plugin_description = esc_html( $plugin_data['Description'] );
                 $plugin_author = esc_html( $plugin_data['Author'] );
@@ -63,6 +86,14 @@ if ( ! class_exists( 'VK_Plugin_List' ) ) {
             $output .= '</table>';
 
             return $output;
+        }
+
+        /**
+         * ショートコードのコールバック関数
+         */
+        public function render_plugin_list() {
+            $plugins = $this->get_filtered_plugins();
+            return $this->render_plugin_list_html( $plugins );
         }
     }
 
